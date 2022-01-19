8761cec19478b210a805adea0bec734f244b32ca
George D Torres <gdavtor@gmail.com>
2022-01-19 17:38:55 -0600

    added some response codes and capped the degree at 20

--- api.py
+++ api.py
@@ -1,8 +1,12 @@
 from flask import Flask, request
-from utils.poly_parse import parse_poly
+import werkzeug
+from utils.poly_parse import parse_poly, parse_opts
 import os
 import json
 import subprocess
+
+MAX_DEG = 20
+ALLOWED_OPTS = {"d","v","t","p","newline"}
 
 app = Flask(__name__)
 
@@ -10,14 +14,39 @@ app = Flask(__name__)
 def factor():
     body = json.loads(request.data.decode('utf-8'))
 
-    poly_str = body['poly']
-    opts = body.get('opts',[])
-    binput = parse_poly(poly_str)
+    # Check for polynomial input
+    try:
+        poly_str = body['poly']
+        binput = parse_poly(poly_str)
+    except Exception as e:
+        raise werkzeug.exceptions.BadRequest(e)
 
+    # Check for options input
+    try:
+        opts = parse_opts(body.get('opts'),ALLOWED_OPTS)
+    except AssertionError as e:
+        raise werkzeug.exceptions.BadRequest(e)
+
+    # Limit degree
+    degree = len(binput.split(",")) - 2
+    if degree  > MAX_DEG:
+        raise werkzeug.exceptions.RequestEntityTooLarge
+
+    # Execute
     print(["./bin/factor_poly",binput]+opts,flush=True)
     proc = subprocess.Popen(["./bin/factor_poly",binput]+opts,stdout=subprocess.PIPE)
     out = proc.communicate()[0]
     return out
+
+
+@app.errorhandler(werkzeug.exceptions.BadRequest)
+def handle_bad_request(e):
+    return f"Bad request: {e}",400
+
+@app.errorhandler(werkzeug.exceptions.RequestEntityTooLarge)
+def handle_poly_too_large(e):
+    return f"Polynomial degree exceeded (maximum degree allowed: {MAX_DEG})",413
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0') 
--- utils/poly_parse.py
+++ utils/poly_parse.py
@@ -1,54 +1,75 @@
-#parse polynomial into csv format
-#e.g. x^4-1  ->  -1,0,0,1
-
 import sys
 
 def parse_poly(polystr):
-	L=polystr.replace("-","+-").split("+")
-	degree=0
-	#regularize terms
-	for i in range(len(L)):
-		term=L[i]
-		term=term.replace('-x','-1*x')
-		if term.startswith('x'):
-			term=term.replace('x','1*x')
-		if term.endswith('x'):
-			term=term.replace('x','x^1')
-		if '*' not in term:
-			term=term.replace('x','*x')
-		L[i]=term
-		#get degree of polynomial
-		d=0
-		try:
-			if("^" in term):
-				d=int(term.split('^')[-1])
-		except ValueError:
-			if len(term)>0:
-				return "error"
-			else:
-				continue
-		if d>degree:
-			degree=d
-	
-	coefs=[0 for i in range(degree+1)]
-	for i in range(len(L)):
-		term=L[i]
-		try:
-			c=int(term)
-			coefs[0]+=c
-		except ValueError:
-			if len(term)>0:
-				d=int(term.split('^')[-1])
-				try:
-					c=int(term.split('*')[0])
-				except ValueError:
-					return "error"
-				coefs[d]+=c
-	rstring=''
-	for c in coefs:
-		rstring+='%d,'%c
+    """
+    parse polynomial into csv format
+    e.g. x^4-1  ->  -1,0,0,1
+    """
+    polystr = polystr.replace(" ","")
+    L=polystr.replace("-","+-").split("+")
+    degree=0
+    #regularize terms
+    for i in range(len(L)):
+        term=L[i]
+        term=term.replace('-x','-1*x')
+        if term.startswith('x'):
+            term=term.replace('x','1*x')
+        if term.endswith('x'):
+            term=term.replace('x','x^1')
+        if '*' not in term:
+            term=term.replace('x','*x')
+        L[i]=term
+        #get degree of polynomial
+        d=0
+        try:
+            if("^" in term):
+                d=int(term.split('^')[-1])
+        except ValueError:
+            if len(term)>0:
+                return "error"
+            else:
+                continue
+        if d>degree:
+            degree=d
 
-	return rstring
+    coefs=[0 for i in range(degree+1)]
+    for i in range(len(L)):
+        term=L[i]
+        try:
+            c=int(term)
+            coefs[0]+=c
+        except ValueError:
+            if len(term)>0:
+                d=int(term.split('^')[-1])
+                try:
+                    c=int(term.split('*')[0])
+                except ValueError:
+                    return "error"
+                coefs[d]+=c
+    rstring=''
+    for c in coefs:
+        rstring+='%d,'%c
+
+    return rstring
+
+def parse_opts(opts_list,allowed_opts):
+    """ Parse options from api request
+    """
+    if opts_list is None:
+        return []
+
+
+    for i in range(len(opts_list)):
+        opt = opts_list[i]
+        if opt[0] != "-":
+            opts_list[i] = "-" + opt
+        o1 = opt.split(" ")[0].replace("-","")
+        assert o1 in allowed_opts,f"Unrecognized option: {opt}"
+
+    
+    rval = " ".join(opts_list)
+    rval = rval.split(" ")
+    return rval
 
 
 if __name__=="__main__":
